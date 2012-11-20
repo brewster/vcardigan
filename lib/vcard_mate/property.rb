bd4808facd5dac3a5ea72371c8449c7187f0925e
Matt Morgan <mlmorg@gmail.com>
2012-11-20 15:28:57 -0600

    Add correct preferred param handling

--- lib/vcard_mate/property.rb
+++ lib/vcard_mate/property.rb
@@ -32,7 +32,6 @@ module VCardMate
         if arg.is_a? Hash
           arg.each do |param, value|
             param = param.to_s.downcase
-            value = value.to_s
             add_param(param, value) unless has_param?(param, value)
           end
         else
@@ -96,9 +95,9 @@ module VCardMate
       property = @group ? "#{@group}.#{name}" : name.upcase
 
       # Params
-      @params.each_with_index do |(key, value), idx|
-        property << ';'
-        property << key.upcase << '=' << value
+      @params.each do |param, value|
+        str = param_to_s(param, value)
+        property << ';' << str if str
       end
 
       # Split with colon
@@ -115,6 +114,18 @@ module VCardMate
 
     private
 
+    def version
+      @vcard.version
+    end
+
+    def v3?
+      version == '3.0'
+    end
+
+    def v4?
+      version == '4.0'
+    end
+
     def add_value(value, idx)
       @values.push(value)
     end
@@ -125,7 +136,6 @@ module VCardMate
 
     def add_param(name, value)
       name = name.to_s.downcase
-      value = value.to_s
       if @params[name]
         # Create array of param values if we have an existing param name
         # present in the hash
@@ -137,6 +147,19 @@ module VCardMate
         # Default is to just set the param to the value
         @params[name] = value
       end
+    end
+
+    def param_to_s(name, value)
+      case name
+      when 'preferred'
+        if !value or value === 0
+          return nil
+        end
+        name = v3? ? 'type' : 'pref'
+        value = v3? ? 'pref' : value.is_a?(Numeric) ? value.to_s : '1'
+      else
+      end
+      name.upcase << '=' << value
     end
 
   end
